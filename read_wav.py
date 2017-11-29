742f40aafe20265046e7b0ab7fc9b4717f889e43
djlopez214 <34042411+djlopez214@users.noreply.github.com>
2017-11-29 07:56:40 -0700

    Update read_wav.py

--- read_wav.py
+++ read_wav.py
@@ -1,6 +1,29 @@
+import scipy.io.wavfile as wavfile
 import numpy as np
 
+def read_wav(filename, strideTms):      # strideTms is the stride in mili seconds
+    rate, data = wavfile.read(filename)
+    N = np.size(data,0)
 
-def read_wav(filename):
-    return None
+    strideT = strideT / 1000    # 10 ms stride
+    strideSamp = (int)(strideT * rate);
+
+    # np.zeros(np.array([1,4]))
+    chan1 = []
+    chan2 = [];
+    for i in range(N):
+      if i%((int)(strideSamp / 2)) == 0:        # assuming 50% overlap
+        # We need to see if this is the right way of getting average
+        avg1 = np.average(data[:,0][i:(i+strideSamp)])
+        avg2 = np.average(data[:,1][i:(i+strideSamp)])
+
+        chan1.append(avg1)
+        chan2.append(avg2)
+  
+
+    
+    
+
+
+
 
