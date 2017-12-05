8376fbb9bc96cbe7c30c5744b9e1033d25d85351
Smith Joshua <joshua.smith4@aggiemail.usu.edu>
2017-12-05 13:28:24 -0700

    matching csv and wav data in matrix

--- read_csv.py
+++ read_csv.py
@@ -1,32 +1,32 @@
 import numpy as np
+import math
 
+def read_csv(filename, time_step=0.05, time_delta=0.5):
+    input_data = np.genfromtxt(
+        filename, dtype=float, delimiter=',', names=True)
 
-def read_csv(filename):
-    input_data = np.genfromtxt(filename, dtype=float, delimiter=',', names=True)
-
-    # Some of the files have different names for this field...
-    # start_time = np.min(input_data['tmi0'])
-    # So just assume all files start at time 0
     start_time = 0
     end_time = np.max(input_data['tmax'])
     duration = end_time - start_time
 
+    samplesPerPoint = int(time_delta / time_step)
+    numDataPoints = math.ceil(duration / time_delta)
 
-    # Convert to a value every <time_step> seconds
-    # This parameter will change the number of elements in the output array
-    # and it should likely be tweaked to try different values
-    time_step = 0.05
-    samples = int(duration / time_step)
-
-    speaker_data = np.zeros(samples)
+    speaker_data = np.zeros((numDataPoints, samplesPerPoint), dtype=int)
 
     active_index = 0
     sample = 0
-    while sample * time_step - start_time < end_time - time_step:
-        if sample * time_step - start_time >= input_data['tmax'][active_index]:
-            active_index += 1
-
-        speaker_data[sample] = input_data['text'][active_index]
-        sample += 1
+    reachedEnd = False
+    for i in range(numDataPoints):
+        for j in range(samplesPerPoint):
+            if active_index >= len(input_data['text']):
+                speaker_data[i,j] = 0
+                continue
+            if (i * samplesPerPoint + j) * time_step - start_time >= input_data['tmax'][active_index]:
+                active_index += 1
+            if active_index >= len(input_data['text']):
+                speaker_data[i,j] = 0
+                continue
+            speaker_data[i,j] = input_data['text'][active_index]
 
     return speaker_data
--- read_wav.py
+++ read_wav.py
@@ -1,35 +1,33 @@
 import scipy.io.wavfile as wavfile
 import numpy as np
 
-def read_wav(filename, strideTms):      # strideTms is the stride in mili seconds
+
+def read_wav(filename, time_delta=0.5, modFunctions=None):
     rate, data = wavfile.read(filename)
-    
-    return data[:,0], data[:,1]             # returning channels that have been averaged  
+    if modFunctions is not None:
+        for func in modFunctions:
+            newRate = func(rate, data)
+            if newRate is not None:
+                rate = newRate
+    samplesPerPoint = int(rate * time_delta)
+    padAmount = samplesPerPoint - (data.shape[0] % samplesPerPoint)
+    res = np.zeros((data.shape[0]+padAmount,data.shape[1]), dtype=data.dtype)
+    return res[:, 0].reshape(-1,samplesPerPoint), res[:, 1].reshape(-1,samplesPerPoint)
 
 
-
-
-
-N = np.size(data,0)
-
-strideT = strideT / 1000    # 10 ms stride
-strideSamp = (int)(strideT * rate);
-
-# np.zeros(np.array([1,4]))
-chan1 = []
-chan2 = [];
-for i in range(N):
-  if i%((int)(strideSamp / 2)) == 0:        # assuming 50% overlap
-    # We need to see if this is the right way of getting average
-    avg1 = np.average(data[:,0][i:(i+strideSamp)])
-    avg2 = np.average(data[:,1][i:(i+strideSamp)])
-
-    chan1avg.append(avg1)
-    chan2avg.append(avg2)
-
-    
-    
-
-
-
-
+# N = np.size(data, 0)
+#
+# strideT = strideT / 1000    # 10 ms stride
+# strideSamp = (int)(strideT * rate)
+#
+# # np.zeros(np.array([1,4]))
+# chan1 = []
+# chan2 = []
+# for i in range(N):
+#     if i % ((int)(strideSamp / 2)) == 0:        # assuming 50% overlap
+#         # We need to see if this is the right way of getting average
+#         avg1 = np.average(data[:, 0][i:(i + strideSamp)])
+#         avg2 = np.average(data[:, 1][i:(i + strideSamp)])
+#
+#         chan1avg.append(avg1)
+#         chan2avg.append(avg2)
